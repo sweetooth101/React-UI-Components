2b2cee9dd5e7a47348e2865e83fd4ab890876186
Victor G <victorgordian103@gmail.com>
2018-09-28 15:27:38 -0700

    Finished the MVP

--- Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardBanner = () => {
+    return <img class ='lambda-image' src ="https://ibin.co/3wnC6SgIOJud.png" alt='lambda-logo' ></img>;
+};
+
+export default CardBanner;
--- Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Day-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+
+const CardContent = () =>{
+ return (<div className='bottom-content'><h1>Get started with React</h1>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+            <p>react.org</p>
+        </div>)
+};
+
+export default CardContent;
